c9f97913dd6d166f5ea099344199307b050a66cd
kluzko <kluzniakkuba@gmail.com>
2023-03-13 15:43:15 +0100

    add header info with favicon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,25 @@
 import styles from "styles/index.module.css";
 import { Menu } from "components/Menu";
 import { Cards } from "components/Cards";
+import Head from "next/head";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <div>
-        <Menu />
+    <>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <title>Welcome in the jungle</title>
+        <link rel="icon" type="image/x-icon" href="/favicon.png" />
+      </Head>
+      <div className={styles.container}>
+        <div>
+          <Menu />
+        </div>
+        <div>
+          <Cards />
+        </div>
       </div>
-      <div>
-        <Cards />
-      </div>
-    </div>
+    </>
   );
 }
